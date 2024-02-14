82da04e1ae4d5fb265609e9a665125721cebd912
Antonio Carrasco <acarrasco0908@gmail.com>
2024-02-14 00:53:39 -0600

    lazy nvim config first commit

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
-require("Antonio")
+require("config")
+require("initlazy")
+
---
+++ lua/config/set.lua
@@ -0,0 +1,34 @@
+vim.opt.guicursor = ""
+vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block'
+vim.o.guicursor = 'r-cr-o:hor20'
+vim.o.guicursor = 'a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+
+vim.opt.nu = true
+vim.opt.relativenumber = true 
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+---vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"

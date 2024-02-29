68ec449e6c8e1d0380b6c2288490b3bf1c500cd4
acarrasco0908 <acarrasco0908@gmail.com>
2024-02-29 00:43:05 -0600

    made changes to telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,42 @@
 -- plugins/telescope.lua:
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	-- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+	'nvim-telescope/telescope.nvim',
+    event = 'VeryLazy',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { -- If encountering errors, see telescope-fzf-native README for install instructions
+            'nvim-telescope/telescope-fzf-native.nvim',
+
+            -- `build` is used to run some command when the plugin is installed/updated.
+            -- This is only run then, not every time Neovim starts up.
+            build = 'make',
+
+            -- `cond` is a condition used to determine whether this plugin should be
+            -- installed and loaded.
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+    },
     config = function()
+        require('telescope').setup {
+            extensions = {
+                ['ui-select'] = {
+                    require('telescope.themes').get_dropdown(),
+                },
+            },
+        }
+
+        -- Enable telescope extensions, if they are installed
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc ='[S]earch [F]iles'})
         vim.keymap.set('n', '<C-p', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ")});
-        end)
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,44 +1,57 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "windwp/nvim-ts-autotag",
+        },
+        config = function()
+            -- import nvim-treesitter plugin
+            local treesitter = require("nvim-treesitter.configs")
+
+            -- configure treesitter
+            treesitter.setup({ -- enable syntax highlighting
+                highlight = {
+                    enable = true,
+                },
+                -- enable indentation
+                indent = { enable = true },
+                -- enable autotagging (w/ nvim-ts-autotag plugin)
+                autotag = {
+                    enable = true,
+                },
+                -- ensure these language parsers are installed
+                ensure_installed = {
+                    "arduino",
+                    "c",
+                    "cmake",
+                    "cpp",
+                    "python",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "gitignore",
+                },
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
+            })
+
+        end,
     },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
-
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "lua",
-          "vim",
-          "gitignore",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
-
-    end,
-  },
 }
